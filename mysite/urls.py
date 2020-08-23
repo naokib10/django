3a2badcc76b2559fa84b780388195be8eeea5d20
bitounaoki <naoki.bitou@gmail.com>
2020-08-23 18:27:06 +0900

    Changed the HTML for the site.

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,11 @@
 
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^polls/', include('polls.urls')),
+#    url(r'^admin/', admin.site.urls),
+#    url(r'^polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
 ]
