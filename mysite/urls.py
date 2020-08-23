9a22a1de1cf399ad89ea5a90df4a0a35e9473f27
bitounaoki <naoki.bitou@gmail.com>
2020-08-23 23:29:10 +0900

    Modified templates to display posts from database.

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,8 +4,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-#    url(r'^admin/', admin.site.urls),
-#    url(r'^polls/', include('polls.urls')),
+    url(r'^admin/', admin.site.urls),
+    url(r'^polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
 ]
